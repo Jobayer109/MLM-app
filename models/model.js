ff5cf895fa1c840fdeb03a7f1dc2a01b6588db21
user.email <jobayer.ahmed109@gmail.com>
2023-02-27 22:59:48 +0600

    Register schema model created

---
+++ models/model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  sponsorId: {
+    type: String,
+    required: [true, "Sponsor id is required"],
+    unique: [true, "Sponsor id should be unique"],
+    trim: true,
+  },
+  sponsorName: {
+    type: String,
+    required: [true, "Sponsor name is required"],
+    unique: [true, "Sponsor name should be unique"],
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: [true, "Full name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email should be unique"],
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
